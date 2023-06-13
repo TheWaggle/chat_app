8f3faf1076a23c70bf28b371c7e61345893f6430
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2023-06-13 11:36:07 +0900

    membersテーブルを追加
    $ mix phx.gen.schema Rooms.Member members account_id:references:accounts room_id:references:rooms

--- lib/chat_app/rooms/room.ex
+++ lib/chat_app/rooms/room.ex
@@ -2,10 +2,14 @@ defmodule ChatApp.Rooms.Room do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias ChatApp.Accounts.Account
+
   schema "rooms" do
     field :room_name, :string
 
     timestamps()
+
+    many_to_many(:accounts, Account, join_through: "members")
   end
 
   @doc false
